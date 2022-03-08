53d3d9ddc3a52cad35728e09ec0e8ca7201d77c3
Apanisile <apanisileifeoluwa@gmail.com>
2022-03-08 21:01:31 +0100

    Built frontend of sentiment Analyser

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import { MuiThemeProvider} from '@material-ui/core/styles';
+import TextField from '@mui/material/TextField';
+import RaisedButton from '@mui/material/Button';
+import Paper from '@mui/material/Paper';
+import Polarity from "./components/Polarity.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const style = {
+    marginLeft: 12,
+};
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            sentence: '',
+            polarity: undefined
+        };
+    };
+
+    analyzeSentence() {
+        fetch('http://localhost:8080/sentiment', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({sentence: this.textField.getValue()})
+        })
+            .then(response => response.json())
+            .then(data => this.setState(data));
+    }
+
+    onEnterPress = e => {
+        if (e.key === 'Enter') {
+            this.analyzeSentence();
+        }
+    };
+
+    render() {
+        const polarityComponent = this.state.polarity !== undefined ?
+            <Polarity sentence={this.state.sentence} polarity={this.state.polarity}/> :
+            null;
+
+        return (
+            <MuiThemeProvider>
+                <div className="centerize">
+                    <Paper zDepth={1} className="content">
+                        <h2>Sentiment Analyser</h2>
+                        <TextField ref={ref => this.textField = ref} onKeyUp={this.onEnterPress.bind(this)}
+                                   hintText="Type your sentence."/>
+                        <RaisedButton  label="Send" style={style} onClick={this.analyzeSentence.bind(this)}/>
+                        {polarityComponent}
+                    </Paper>
+                </div>
+            </MuiThemeProvider>
+        );
+    }
 }
 
 export default App;
