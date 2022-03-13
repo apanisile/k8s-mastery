ab2f96f351701d8257e8c1efff0e44594bb44d8a
Apanisile <apanisileifeoluwa@gmail.com>
2022-03-13 10:49:54 +0100

    Removed scripts

--- jenkins/scripts/kill.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash sh
-
-echo 'The following command terminates the "npm start" process using its PID'
-echo '(written to ".pidfile"), all of which were conducted when "deliver.sh"'
-echo 'was executed.'
-set -x
-kill $(cat .pidfile)
